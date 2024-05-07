f0cf204a15ccd85c1a3e8b8c7672c093654d0990
Miguel <miguelmzastrow@gmail.com>
2024-05-06 23:49:10 -0300

    conexao php

---
+++ conexao.php
@@ -0,0 +1,7 @@
+<?php
+    $servidor= 'localhost';
+    $usuario= 'root';
+    $senha= '';
+    $banco= 'banco_clientes';
+    $conexao= mysqli_connect($servidor,$usuario,$senha,$banco_clientes);
+?>
